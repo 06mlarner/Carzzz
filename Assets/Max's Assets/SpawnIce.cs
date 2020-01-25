3d788bee68bce4ed7a29aa15d8f1228a77f6be59
06mlarner <06mlarner@brightoncollege.net>
2020-01-25 11:05:42 +0000

    Initial

---
+++ Assets/Max's Assets/SpawnIce.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class SpawnIce : MonoBehaviour
+{
+  public Transform spawnPoint;
+  public GameObject prefab;
+
+  void OnTriggerEnter(Collider other) {
+    
+    Debug.Log("trigger entered");
+    
+    Instantiate(prefab, spawnPoint.position, spawnPoint.rotation);
+    
+    Debug.Log("object spawned");
+  }
+}
